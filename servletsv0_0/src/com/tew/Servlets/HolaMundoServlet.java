ba0213e56e02746cf080394da21c978e161279b7
uo257776 <uo257776@GI-AN-S3_11.aulasuo.uniovi.es>
2019-09-20 19:56:14 +0200

    TEW - 2.2 Servelts con sesiones.

--- servletsv0_0/src/com/tew/Servlets/HolaMundoServlet.java
+++ servletsv0_0/src/com/tew/Servlets/HolaMundoServlet.java
@@ -2,6 +2,7 @@ package com.tew.Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Vector;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,7 +30,7 @@ public class HolaMundoServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String nombre = (String) request.getParameter("NombreUsuario");
+
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
 				
@@ -38,9 +39,29 @@ public class HolaMundoServlet extends HttpServlet {
 		out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
 		out.println("<BODY>");
 		
-		if (nombre != null) out.println("<br>Hola " + nombre + "<br>");
+		String nombre = (String) request.getParameter("NombreUsuario");
 		
+		@SuppressWarnings("unchecked")
+		Vector<String> listado = (Vector<String>) request.getSession().getAttribute("listado");
+		
+		if (listado==null) {
+			listado = new Vector<String>();
+		}
+		
+		if (nombre != null) {
+			out.println("<br>Hola " + nombre + "<br>");
+			listado.addElement(nombre);
+		}
+		
+		request.getSession().setAttribute("listado", listado);
+
 		out.println("Bienvenido a mi primera página web!");
+		out.println("<br>");
+		out.println("Contigo, hoy me han visualizado:<br>");
+		for (int i=0; i < listado.size(); i++) {
+			out.println("<br>" + (String)listado.elementAt(i));
+		}
+		out.println("<center><a href=\"index.html\">volver</a></center>");
 		out.println("</BODY></HTML>");
 	}
 
