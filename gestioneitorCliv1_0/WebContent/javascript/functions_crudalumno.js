3379db5f6c34fac75185fa0c3a0df5261e9d8c16
Ryanor <Ryanor@Ryanor-PC>
2019-11-29 12:02:01 +0100

    TEW - 9.7 Controller y eventos

--- gestioneitorCliv1_0/WebContent/javascript/functions_crudalumno.js
+++ gestioneitorCliv1_0/WebContent/javascript/functions_crudalumno.js
@@ -2,60 +2,54 @@
 function Model() {
 	// Lista de alumnos.
 	this.tbAlumnos = null;
-};
 
-// Carga los datos del servicio sobreescribiendo el dato this.tbAlumnos.
-this.load = function() {
-	this.tbAlumnos = AlumnosServicesRs.getAlumnos();
-}
-
-// Llamamos al servicio de alta de alumno
-this.add = function(alumno) {
-	// Llamamos al servicio de alta de alumno
-	AlumnosServicesRs.saveAlumno({
-		$entity : alumno,
-		$contentType : "application/json"
-	});
-	// Recargamos la lista de alumnos.
-	this.load();
-}
-
-// Actualización de un alumno existente: PENDIENTE DE IMPLEMENTAR
-this.edit = function(alumno) {
-	// Llamamos al servicio de actualización de alumno
-	AlumnosServicesRs.updateAlumno({
-		$entity : alumno,
-		$contentType : "application/json"
-	});
-	// Recargamos la lista de alumnos.
-	this.load();
-}
-
-// Eliminación un alumno existente
-this.remove = function(id_alumno) {
-	// Llamamos al servicio de borrado de alumno
-	AlumnosServicesRs.deleteAlumno({
-		id : id_alumno
-	});
-	// Recargamos la lista de alumnos.
-	this.load();
-}
-
-this.find = function(id_alumno) {
-	function checkAlumno(obj) {
-		return obj.id == id_alumno;
+	// Carga los datos del servicio sobreescribiendo el dato this.tbAlumnos.
+	this.load = function() {
+		this.tbAlumnos = AlumnosServicesRs.getAlumnos();
 	}
-	// Buscamos los datos del alumno cuyo id_alumno sea el mismo que el
-	// seleccionado
-	var alumno = this.tbAlumnos.find(checkAlumno);
-	return alumno;
-}
 
-$(function() {
-	// Creamos el modelo con los datos y la conexión al servicio web.
-	var model = new Model();
-	model.load();
-})
+	// Llamamos al servicio de alta de alumno
+	this.add = function(alumno) {
+		// Llamamos al servicio de alta de alumno
+		AlumnosServicesRs.saveAlumno({
+			$entity : alumno,
+			$contentType : "application/json"
+		});
+		// Recargamos la lista de alumnos.
+		this.load();
+	}
+
+	// Actualización de un alumno existente: PENDIENTE DE IMPLEMENTAR
+	this.edit = function(alumno) {
+		// Llamamos al servicio de actualización de alumno
+		AlumnosServicesRs.updateAlumno({
+			$entity : alumno,
+			$contentType : "application/json"
+		});
+		// Recargamos la lista de alumnos.
+		this.load();
+	}
+
+	// Eliminación un alumno existente
+	this.remove = function(id_alumno) {
+		// Llamamos al servicio de borrado de alumno
+		AlumnosServicesRs.deleteAlumno({
+			id : id_alumno
+		});
+		// Recargamos la lista de alumnos.
+		this.load();
+	}
+
+	this.find = function(id_alumno) {
+		function checkAlumno(obj) {
+			return obj.id == id_alumno;
+		}
+		// Buscamos los datos del alumno cuyo id_alumno sea el mismo que el
+		// seleccionado
+		var alumno = this.tbAlumnos.find(checkAlumno);
+		return alumno;
+	}
+};
 
 // Clase que contiene la gestión de la capa Vista
 function View() {
@@ -109,5 +103,63 @@ function View() {
 		var id_alumno = parseInt(celda.closest('tr').find('td').get(1).innerHTML);
 		return id_alumno;
 	}
+
 };
 
+function Controller(varmodel, varview) {
+	// Definimos una copia de this para evitar el conflicto con el this (del
+	// objeto que recibe el evento)
+	// en los manejadores de eventos
+	var that = this;
+	// referencia al modelo
+	this.model = varmodel;
+	// refefencia a la vista
+	this.view = varview;
+	// Funcion de inicialización para cargar modelo y vista, definición de
+	// manejadores
+	this.init = function() {
+		// Cargamos la lista de alumnos del servicio
+		this.model.load();
+		// Repintamos la lista de alumnos.
+		this.view.list(this.model.tbAlumnos);
+
+		// Manejador del botón submit del formulario de Alta y Edición
+		$("#frmCRUDAlumnos").bind("submit",
+		// Método que gestiona el evento de clickar el botón submit del
+		// formulario
+		function(event) {
+			// Si el alumno cargado en el formulario tiene ID se invoca al
+			// método de edición
+			// sino se invoca al método de alta.
+			alumno = that.view.loadAlumnoFromForm();
+			if ($("#id").val() == "") {
+				that.model.add(alumno);
+			} else {
+				that.model.edit(alumno);
+			}
+			// Volvemos a listar los alumnos restantes para que aparezca el
+			// nuevo
+			// alumnos o el editado
+			that.view.list(that.model.tbAlumnos);
+		});
+
+		// Manejador del enlace de edición de un alumno en la tabla
+		$("#tblList").on("click", ".btnEdit",
+		// Método que gestiona el evento de clickar en el evento
+		function(event) {
+			// Obtenemos el id del alumno seleccionado mediante el icono de
+			// edición
+			var id_alumno = that.view.getIdAlumno($(this));
+			// Obtenemos el alumno con el id_alumno
+			var alumno = that.model.find(id_alumno);
+			// Cargamos el formulario con los datos del alumno seleccionado
+			// para
+			// editar
+			that.view.loadAlumnoInForm(alumno);
+		});
+
+		$("#tblList").on("click", ".btnReset", function(event) {
+			that.view.list();
+		});
+	}
+};
