c2adeb2fa1cc9376e07ae7ba9e1de66626ced3ad
Ryanor <Ryanor@Ryanor-PC>
2019-11-29 11:40:55 +0100

    TEW - 9.4 View - Método list

--- gestioneitorCliv1_0/WebContent/javascript/functions_crudalumno.js
+++ gestioneitorCliv1_0/WebContent/javascript/functions_crudalumno.js
@@ -52,7 +52,39 @@ this.find = function(id_alumno) {
 }
 
 $(function() {
-	 //Creamos el modelo con los datos y la conexión al servicio web.
-	 var model = new Model();
-	 model.load();
-	}) 
+	// Creamos el modelo con los datos y la conexión al servicio web.
+	var model = new Model();
+	model.load();
+})
+
+// Clase que contiene la gestión de la capa Vista
+function View() {
+	this.list = function(lista) {
+		$("#tblList").html("");
+		$("#tblList").html(
+				"<thead>" + "<tr>" + "<th></th>" + "<th>ID</th>"
+						+ "<th>IDUser</th>" + "<th>Nombre</th>"
+						+ "<th>Apellidos</th>" + "<th>Email</th>" + "</tr>"
+						+ "</thead>" + "<tbody>" + "</tbody>");
+		for ( var i in lista) {
+			var alumno = lista[i];
+			$("#tblList tbody")
+					.append(
+							"<tr> <td>"
+									+ "<img src='icons/edit.png' class='btnEdit'/>"
+									+ "<img src='icons/delete.png' class='btnDelete'/> </td>"
+									+ "<td>" + alumno.id + "</td>" + "<td>"
+									+ alumno.iduser + "</td>" + "<td>"
+									+ alumno.nombre + "</td>" + "<td>"
+									+ alumno.apellidos + "</td>" + "<td>"
+									+ alumno.email + "</td></tr>");
+		}
+	}
+	this.loadAlumnoFromForm = function() {
+	} // PENDIENTE DE IMPLEMENTAR
+	this.loadAlumnoInForm = function(alumno) {
+	} // PENDIENTE DE IMPLEMENTAR
+	this.getIdAlumno = function(celda) {
+	} // PENDIENTE DE IMPLEMENTAR
+};
+
